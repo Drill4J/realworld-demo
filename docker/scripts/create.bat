f60c646269ed1d4758cc6b9015af0bf571d13179
Bohdan_Asieiev <aseevbogdan5@gmail.com>
2023-09-26 12:39:04 +0300

    fix: remove a create script

--- docker/scripts/create.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo "Turn on admin part"
-docker-compose -f ../docker-compose-admin.yml up -d
-ping 127.0.0.1 -n 6 > nul
-echo "Turn on real-world_example part"
-docker-compose -f ../docker-compose-app.yml up -d
