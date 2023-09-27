fce3eaf711f0ffb7c1f74eaab80e761ef8c0065d
RomanDavlyatshin <davlyatshinra@gmail.com>
2023-09-27 20:50:13 +0400

    chore: add check to skip download if chromium/chromedriver binaries already exist

--- chrome-driver-setup.sh
+++ chrome-driver-setup.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 chromeDriverUrl="https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_win32.zip"
+destinationDir="./chrome-driver"
+zipFile="$destinationDir/chromedriver_win32.zip"
+
 # 1. Ensure dir exists
-mkdir -p ./chrome-driver
-# 2. Download
-curl -L -o ./chrome-driver/chromedriver_win32.zip "$chromeDriverUrl"
-# 3. Unzip
-unzip -d ./chrome-driver/ ./chrome-driver/chromedriver_win32.zip
-# 4. Remove zip file
-rm ./chrome-driver/chromedriver_win32.zip
+mkdir -p "$destinationDir"
+
+# 2. Check if the unzipped files already exist
+if [ ! -f "$destinationDir/chromedriver.exe" ]; then
+    # 3. Download
+    curl -L -o "$zipFile" "$chromeDriverUrl"
+
+    # 4. Unzip
+    unzip -d "$destinationDir" "$zipFile"
+
+    # 5. Remove zip file
+    rm "$zipFile"
+else
+    echo "Chromedriver already exists in $destinationDir. Skipping download."
+fi
--- chromium-setup.sh
+++ chromium-setup.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 chromeBinaryUrl="https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Win_x64%2F1148119%2Fchrome-win.zip?generation=1684876145913156&alt=media"
+destinationDir="./chromium-binary"
+zipFile="$destinationDir/chrome-win.zip"
+
 # 1. Ensure dir exists
-mkdir -p ./chromium-binary
-# 2. Download
-curl -L -o ./chromium-binary/chrome-win.zip "$chromeBinaryUrl"
-# 3. Unzip
-unzip -d ./chromium-binary/ ./chromium-binary/chrome-win.zip
-# 4. Remove zip file
-rm ./chromium-binary/chrome-win.zip
+mkdir -p "$destinationDir"
+
+# 2. Check if the unzipped files already exist
+if [ ! -f "$destinationDir/chrome-win/chrome.exe" ]; then
+    # 3. Download
+    curl -L -o "$zipFile" "$chromeBinaryUrl"
+    
+    # 4. Unzip
+    unzip -d "$destinationDir" "$zipFile"
+    
+    # 5. Remove zip file
+    rm "$zipFile"
+else
+    echo "Chromium binary already exists in $destinationDir. Skipping download."
+fi
