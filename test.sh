fab516f7354e0f603dbf02f292b2eb87d81bb3f0
RomanDavlyatshin <davlyatshinra@gmail.com>
2024-08-21 21:59:47 +0400

    feat: init drill DB with API key + init metabase DB with tables

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export DRILL_API_KEY="1_1d5e9bc750d3190dece821c66cbb3610b054154c1232e512f8f7f78ee5ae15e6"
+export DRILL_API_KEY="1_01dce51ff20544ff621eae09c725a4eb50d9fea30251c09d4c17764c343ebea4"
 export DRILL_API_URL="http://localhost:8090/api"
 
 ./gradlew clean :$1:test
